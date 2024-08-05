aa973b88ea0fdef090c4248cb5a1a3e7ef319b6c
MadgulaVamshi06 <vamshimadgula@gmail.com>
2024-08-05 23:01:02 +0530

    initial commit

---
+++ src/assets/index.js
@@ -0,0 +1,21 @@
+
+import bannerImg from "./images/bannerImg.png";
+import testimonialOne from "./images/testmonial/testimonialOne.png";
+import testimonialTwo from "./images/testmonial/testimonialTwo.png";
+import quote from "./images/testmonial/quote.png";
+import contactImg from "./images/contact/contactImg.png";
+import photo from "./images/photo.png"
+import croma from "./images/projects/croma.png";
+import jumia from "./images/projects/jumia.png";
+import mg from "./images/projects/1mg.png";
+export {
+  photo,
+  bannerImg,
+  croma,
+  jumia,
+  mg,
+  testimonialOne,
+  testimonialTwo,
+  quote,
+  contactImg,
+};
